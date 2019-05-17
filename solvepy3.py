8767efd1479207a5248da273264ebb4fea85d720
template97 <gogodbwngud@naver.com>
2019-05-18 02:32:47 +0900

    resolution but horrible perf

--- solvepy3.py
+++ solvepy3.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import operator
 import time
+import copy
 
 VAR_NUM = 0
 CLS_NUM = 0
@@ -21,28 +22,37 @@ def main(num):
 	FA = []
 	(VAR_NUM, CLS_NUM, _input) = get_input(num)
 
-	FA = _input[:]
+	#FA = copy.deepcopy(_input)
+	FA = _input
 
 	(result, __FA, __A) = DPLL(FA, [], 0)
 
 	if not result:
-		print("s UNSATISFIABLE")
+		print("s UNSATISFIABLE@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 	else: 
 		print("s SATISFIABLE")
 		for a in __A:
 			print(a, end = ' ')
 
+
 def DPLL(FA, A, lev):
 	global VAR_NUM, CLS_NUM
-	# db = False
-	# if(db): print("\nDPLL start", A, "\n",FA)
+	db = True
+	# print("\nDPLL start", A, "\n",FA, "\n")
+
+	if FA == []:
+		return (True, FA, A)
 
 	FA_temp = []
+	A_temp = []
 
-	new_FA = FA[:]
+	new_FA = copy.deepcopy(FA)
 	new_A = A[:]
-	while(FA_temp != new_FA):
-		FA_temp = new_FA[:]
+
+	at_least = True
+	while(at_least or A_temp != new_A): #FA_temp != new_FA):
+		#FA_temp = copy.deepcopy(new_FA)
+		A_temp = new_A[:]
 		pure = []
 		count = []
 		(pure, count) = variable_count(new_FA, new_A)			# count variable appearing
@@ -55,29 +65,96 @@ def DPLL(FA, A, lev):
 			# if(db): print("good 1")
 			return (True, new_FA, new_A)
 
+
+
 		(result, new_FA, new_A) = unit_propagation(new_FA, new_A)			# threat unit clause
 		if not result:
 			# if(db): print("wrong 2")
-			return (False, FA, A)
+			return (False, new_FA, new_A)
 		elif(new_FA == []):
 			# if(db): print("good 2")
 			return (True, new_FA, new_A)
 
+		if not is_res_ok(new_FA):
+			#print("resolution fail")#, new_FA)
+			return (False, new_FA, new_A)
+
+		at_least = False
+
 	# if(db): print("\nafter propagation", new_A, "\n", new_FA)
+	
+	#_FA = new_FA[:]
+	#_A = new_A[:]
+
+	# if not is_res_ok(new_FA):
+	# 	print("resolution fail2")#, new_FA)
+	# 	return (False, new_FA, new_A)
 	return select_and_branch(new_FA, new_A, count, lev)
 
-def select_and_branch(_FA, _A, count, lev):
+
+def is_res_ok(FA): # check completeness
+	global VAR_NUM, CLS_NUM
+
+	new_FA = copy.deepcopy(FA)
+
+	
+	res = True
+	while(True):
+		if len(new_FA) == 1:
+			return True
+		if not res:
+			return True
+
+		res = False
+		exist = [-1 for j in range(VAR_NUM * 2 + 2)]
+
+		i = 0
+		for clause in new_FA:
+			if clause == []:
+				return False
+			for var in clause:
+				if exist[VAR_NUM + 1 + (-1 * var)] != -1:
+					resolution(clause, new_FA[exist[VAR_NUM + 1 + (-1 * var)]], var, new_FA)
+					res = True
+					break
+				exist[VAR_NUM + 1 + var] = i
+			if res:
+				break
+			i += 1
+
+	return True
+
+def resolution(cls1, cls2, var, FA):
+	#n = exist[VAR_NUM + 1 + (-1 * var)]
+
+
+	cls1.remove(var)
+	cls2.remove(-1 * var)
+
+	#__cls = cls2
+	_cls = cls2[:]
+	for v in _cls:
+		if v not in cls1:
+			cls1.append(v)
+	#cls1.extend(_cls)
+	FA.remove(cls2)
+
+
+def select_and_branch(FA, A, count, lev):
 	global VAR_NUM, CLS_NUM
 	# db = False
 	# if(db): 
-	print(lev, end = ' ')
+	
+	#print(len(A), end = ' ')
+
+	# print(_FA, _A)
 	max_cnt = 0
 	min_cnt = 0
 	selected_var = 0
 	selected_cnt = 0
 	
 	for (_var, _cnt) in count.items():
-		if( _var not in _A and (-1 * _var) not in _A and _var != 0):
+		if( _var not in A and (-1 * _var) not in A and _var != 0):
 			#print(_var, end = ' ')
 			if(_cnt >= max_cnt):
 				max_cnt = _cnt
@@ -92,33 +169,29 @@ def select_and_branch(_FA, _A, count, lev):
 					selected_cnt = _cnt
 	
 	if( selected_cnt >= 0 ):
-		var = selected_var
-	elif( selected_cnt < 0):
 		var = selected_var * -1
+	elif( selected_cnt < 0):
+		var = selected_var #* -1
 
-	FA = _FA[:]
-	A = _A[:]
+	# FA = copy.deepcopy(_FA)
+	# A = _A[:]
 	new_FA = []
 	new_A = []
 	(result, new_FA, new_A) = add_and_check(FA, A, var)
 	if result:
-		# if(db): print("[", var, "]")
 		(new_result, new_FA, new_A) = DPLL(new_FA, new_A, lev + 1)
 		if new_result:
-			# if(db): print("select : ", var, "succeed")
 			# if(db): print("good 3")
 			return (new_result, new_FA, new_A)
 
-	FA = _FA[:]
-	A = _A[:]
+	# FA = copy.deepcopy(_FA)
+	# A = _A[:]
 	new_FA = []
 	new_A = []
 	(result, new_FA, new_A) = add_and_check(FA, A, (-1 * var))
 	if result:
-		# if(db): print("[", (-1 * var), "]")
 		(new_result, new_FA, new_A) = DPLL(new_FA, new_A, lev +1)
 		if new_result:
-			# if(db): print("select : ", (-1 *var), "succeed")
 			# if(db): print("good 4")
 			return (new_result, new_FA, new_A)
 
@@ -160,7 +233,7 @@ def pure_literal_elimination(FA, A, pure):
 	# db = False
 
 	result = True
-	new_FA = FA[:]
+	new_FA = copy.deepcopy(FA)
 	new_A = A[:]
 	for var in range(1, VAR_NUM+1):
 		if(pure[var] == P_POS):
@@ -175,18 +248,18 @@ def pure_literal_elimination(FA, A, pure):
 
 	return (result, new_FA, new_A)
 
-def unit_propagation(FA, A):
+def unit_propagation(new_FA, new_A):
 	global VAR_NUM, CLS_NUM
 	# db = False
 
-	new_FA = FA[:]
-	new_A = A[:]
+	# new_FA = copy.deepcopy(FA)
+	# new_A = A[:]
 	result = True
 
-	for clause in FA:
+	for clause in new_FA:
 		if len(clause) == 1:
-			# if(db): print(clause, "is unit clause")#" of ", FA)
-			(result, new_FA, new_A) = add_and_check(FA, A, clause[0])
+			# if(db): print(clause, "is unit clause", " of ", A)
+			(result, new_FA, new_A) = add_and_check(new_FA, new_A, clause[0])
 			return (result, new_FA, new_A)
 
 	return (result, new_FA, new_A)
@@ -197,7 +270,7 @@ def apply_A_by_guess(_FA, _A):
 	# db = False
 
 	new_FA = []
-	FA = _FA[:]
+	FA = copy.deepcopy(_FA)
 	A = _A[:]
 	# if(db): print("apply_A_by_guess", FA, A)
 
@@ -229,24 +302,33 @@ def add_and_check(_FA, _A, num):
 	global VAR_NUM, CLS_NUM
 	db = False
 
-	FA = _FA[:]
-	A = _A[:]
+	#FA = _FA[:]	
 	# if(db): print("add_and_check", A, num)
 
-
-	A.append(num)
-	A = list(set(A))
-
-	temp = []
-	for i in A:
-		temp.append(abs(i))
-	temp = list(set(temp))
-
-	# if(db): print(A, temp)
-	if( len(A) > len(temp)):
-		return (False, FA, A)
+	if num in _A:
+		print("error")
+		exit(-1)
+	elif (-1 * num) in _A:
+		print(num, _A)
+		return (False, _FA, _A)
 	else:
-		return apply_A_by_guess(FA, A)
+		A = _A[:]
+		A.append(num)
+		# print("add and check:",num, _FA)
+		return apply_A_by_guess(_FA, A)
+	
+	#A = list(set(A))
+
+	# temp = []
+	# for i in A:
+	# 	temp.append(abs(i))
+	# temp = list(set(temp))
+
+	# # if(db): print(A, temp)
+	# if( len(A) > len(temp)):
+	# 	return (False, FA, A)
+	# else:
+	# 	return 
 
 
 def get_input(num):
@@ -315,9 +397,10 @@ if __name__ == '__main__':
 	if len(sys.argv) != 1:
 		main(0)
 	else:
-		for i in range(2,3):
-			print(i, end = ' ')
+		for i in range(1, 6):
+			print(i, end = ': ')
 			startTime = time.time()
 			main(i)
 			endTime = time.time() - startTime
-			print("\ntime:", endTime) 
+			print("\ntime:", endTime)
+			print("")				
